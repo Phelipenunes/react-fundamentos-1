5a095f0e796e48976330275bc56c8424ce581462
Phelipe <phelipesenac@gmail.com>
2024-01-15 11:58:31 -0300

    15/01/24
    .

--- src/pages/Produtos.jsx
+++ src/pages/Produtos.jsx
@@ -1,21 +1,46 @@
+import { useEffect } from "react";
 import { useState } from "react";
 
 function Produtos() {
-  const [prosutos, setProdutos] = useState([]);
   /* o state "produtos" é iniciado como um array vazio posteriomente (após o carregamento dos dados da API), ele será preenchido com os objetos */
-  const carregarDados = async () => {
-    try {
-      const resposta = await fetch(`https://fakestoreapi.com/products`);
-      const dados = await resposta.json();
-      console.log(dados);
-      setProdutos(dados);
-    } catch (error) {
-      console.error("Houve um erro: " + error);
-    }
-  };
+  const [produtos, setProdutos] = useState([]);
 
-  carregarDados();
-  return <h2>Produtos</h2>;
+  const [loading, setLoading] = useState(true);
+
+  /* Gerenciando o efeito colateral do componentepara o carregamento dos dados da API */
+  useEffect(() => {
+    const carregarDados = async () => {
+      try {
+        const resposta = await fetch(`https://fakestoreapi.com/products`);
+        const dados = await resposta.json();
+        console.log(dados);
+        setProdutos(dados);
+        setLoading(false);
+      } catch (error) {
+        console.error("Houve um erro: " + error);
+      }
+    };
+    carregarDados();
+  }, []);
+
+  return (
+    <article>
+      <h2>Produtos</h2>
+      {loading ? (
+        <p>Carregando dados</p>
+      ) : (
+        produtos.map((produtos) => {
+          return (
+            <section key={produtos.id}>
+              <h3>{produtos.title}</h3>
+              <p>Preço: {produtos.price}</p>
+              <p>descrição: {produtos.description}</p>
+            </section>
+          );
+        })
+      )}
+    </article>
+  );
 }
 
 export default Produtos;
