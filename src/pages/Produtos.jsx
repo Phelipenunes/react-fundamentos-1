a0b9f00beb541ae040068e85c44dcba4e0e86266
Phelipe <phelipesenac@gmail.com>
2024-01-15 11:11:55 -0300

    1111
    uso do usestate

--- src/pages/Produtos.jsx
+++ src/pages/Produtos.jsx
@@ -1,4 +1,20 @@
+import { useState } from "react";
+
 function Produtos() {
+  const [prosutos, setProdutos] = useState([]);
+  /* o state "produtos" é iniciado como um array vazio posteriomente (após o carregamento dos dados da API), ele será preenchido com os objetos */
+  const carregarDados = async () => {
+    try {
+      const resposta = await fetch(`https://fakestoreapi.com/products`);
+      const dados = await resposta.json();
+      console.log(dados);
+      setProdutos(dados);
+    } catch (error) {
+      console.error("Houve um erro: " + error);
+    }
+  };
+
+  carregarDados();
   return <h2>Produtos</h2>;
 }
 
